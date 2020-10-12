f88858ac5cd6024cd9a37c359e57a582a8a25e27
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-12 15:30:13 +0200

    Added api endpoint to create todo item

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,54 @@
+const { request, response } = require('express');
+const { error } = require('firebase-functions/lib/logger');
+const { db } = require('../utils/admin');
+
 exports.getAllTodos = (request, response) => {
-    todos = [
-        {
-            'id': '1',
-            'title': 'greeting',
-            'body': 'Hello world from Mathews'
-        },
-        {
-            'id': '2',
-            'title': 'greeting2',
-            'body': 'Hello world from Miyo'
-        }
-    ];
-    return response.json(todos);
+    db
+    .collection('todos')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+        let todos = [];
+        data.forEach((document) => {
+            todos.push({
+                todoId: document.id,
+                title: document.data().title,
+                body: document.data().body,
+                createdAt: document.data().createdAt
+            });
+        });
+        return response.json(todos);
+    })
+    .catch((error) => {
+        console.error(error);
+        return response.status(500).json({ error: error.code });
+    });
+};
+
+exports.postOneTodo = (request, response) => {
+    if (request.body.body.trim() === '') {
+        return response.status(400).json({ body: 'Must not be empty' });
+    }
+
+    if (request.body.title.trim() === '') {
+        return response.status(400).json({ title: 'Must not be empty' });
+    }
+
+    const newTodoItem = {
+        title: request.body.title,
+        body: request.body.body,
+        createdAt: new Date().toISOString()
+    };
+    db
+    .collection('todos')
+    .add(newTodoItem)
+    .then((document) => {
+        const responseTodoItem = newTodoItem;
+        responseTodoItem.id = document.id;
+        return response.json(responseTodoItem);
+    })
+    .catch((error) => {
+        response.status(500).json({ error: 'Something went wrong' });
+        console.error(error);
+    })
 }
