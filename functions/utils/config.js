39d2ef2d383d131445672c3394f8cddf842a1ec4
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-12 16:34:24 +0200

    Added firebase config file

---
+++ functions/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBx5zaiGNRYOTITrOfdxtls3_rPm_UMP3s",
+    authDomain: "credit-score-app-mcn95.firebaseapp.com",
+    databaseURL: "https://credit-score-app-mcn95.firebaseio.com",
+    projectId: "credit-score-app-mcn95",
+    storageBucket: "credit-score-app-mcn95.appspot.com",
+    messagingSenderId: "974537745206",
+    appId: "1:974537745206:web:7701e4c00b216b551e8997"
+  };
