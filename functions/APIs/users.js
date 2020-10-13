4047e0efd94a34a5875127f8a6069be2d80e4601
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-13 09:38:05 +0200

    Added user CRUD functions and profile upload

--- functions/APIs/users.js
+++ functions/APIs/users.js
@@ -98,3 +98,92 @@ exports.signUpUser = (request, response) => {
         }
     });
 };
+
+deleteImage = (imageName) => {
+    const bucket = admin.storage().bucket();
+    const path = `${imageName}`;
+    return bucket.file(path).delete().then(() => {
+        return;
+    }).catch(error => {
+        return;
+    })
+}
+
+exports.uploadProfilePhoto = (request, response) => {
+    const Busboy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const busboy = new Busboy({ headers: request.headers });
+
+    let imageFileName;
+    let imageToBeUploaded = {};
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        if (mimetype !== 'image/png' && mimetype !== 'image/jpeg') {
+            return response.status(400).json({ error: 'Wrong file type submitted' });
+        }
+        const imageExtension =filename.split('.')[filename.split('.').length-1];
+        imageFileName = `${request.user.username}.${imageExtension}`;
+        const filePath = path.join(os.tmpdir(), imageFileName);
+        imageToBeUploaded = { filePath, mimetype };
+        file.pipe(fs.createWriteStream(filePath));
+    });
+    deleteImage(imageFileName);
+    busboy.on('finish', () => {
+        admin
+        .storage()
+        .bucket()
+        .upload(imageToBeUploaded.filePath, {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contentType: imageToBeUploaded.mimetype
+                }
+            }
+        })
+        .then(() => {
+            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+            return db.doc(`users/${request.user.username}`).update({
+                imageUrl
+            });
+        })
+        .then(() => {
+            return response.json({ message: 'Image uploaded successfully' });
+        })
+        .catch(err => {
+            console.error(err);
+            return response.status(400).json({ error: err.code });
+        });
+    });
+    busboy.end(request.rawBody);
+}
+
+exports.getUserDetails = (request, response) => {
+    let userData = {};
+    db
+    .doc(`/users/${request.user.username}`)
+    .get()
+    .then(doc => {
+        if (doc.exists) {
+            userData.userCredentials = doc.data();
+            return response.json(userData);
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        return response.status(500).json({ error: err.code });
+    })
+}
+
+exports.updateUserDetails = (request, response) => {
+    let document = db.collection('users').doc(`${request.user.username}`);
+    document.update(request.body).then(() => {
+        response.json({ message: 'Updated successfully' });
+    })
+    .catch(err => {
+        console.error(err);
+        return response.status(500).json({ error: err.code });
+    })
+}
