5aaa43227ded7b364912b56a50bca5fe45da2cb7
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-13 09:37:20 +0200

    Added authentication to the todo apis

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const app = require('express')();
+const auth = require('./utils/auth');
 
 const {
     getAllTodos,
@@ -10,14 +11,21 @@ const {
 
 const {
     loginUser,
-    signUpUser
+    signUpUser,
+    uploadProfilePhoto,
+    getUserDetails,
+    updateUserDetails
 } = require('./APIs/users');
 
-app.get('/todos', getAllTodos);
-app.post('/todos', postOneTodo);
-app.delete('/todos/:todoId', deleteTodo);
-app.put('/todos/:todoId', editTodo);
+
+app.get('/todos', auth, getAllTodos);
+app.post('/todos', auth, postOneTodo);
+app.delete('/todos/:todoId', auth, deleteTodo);
+app.put('/todos/:todoId', auth, editTodo);
 
 app.post('/users/login', loginUser);
 app.post('/users/signup', signUpUser);
+app.post('/users/image', auth, uploadProfilePhoto);
+app.get('/user', auth, getUserDetails);
+app.put('/user', auth, updateUserDetails);
 exports.api = functions.https.onRequest(app);
