42d7a35b1118d5c3ea08034435b37be514bd2708
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-13 12:16:35 +0200

    Added package to handle cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const app = require('express')();
+const cors = require('cors');
 const auth = require('./utils/auth');
+
+app.use(cors());
 
 const {
     getAllTodos,
