43aaca706bbb134424821a9b9452d826e471aaee
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-10-13 07:15:01 +0200

    Added user signup and login

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,16 @@ const {
     editTodo
 } = require('./APIs/todos');
 
+const {
+    loginUser,
+    signUpUser
+} = require('./APIs/users');
+
 app.get('/todos', getAllTodos);
 app.post('/todos', postOneTodo);
 app.delete('/todos/:todoId', deleteTodo);
 app.put('/todos/:todoId', editTodo);
+
+app.post('/users/login', loginUser);
+app.post('/users/signup', signUpUser);
 exports.api = functions.https.onRequest(app);
